d5ca49872f59ff29a2f4b19f7dbb9dd2a72ffca3
anton nenashev <psihonort@gmail.com>
2022-03-26 22:14:33 +0600

    update

---
+++ impl/request/impl/__init__.py
@@ -0,0 +1,1 @@
+from .ProductRequestFactoryStoreFactoryImpl import RequestProductFactoryStoreFactoryImpl
---
+++ impl/tasks_scraper_manager/impl/__init__.py
@@ -0,0 +1,1 @@
+from .TaskScraperManagerFactoryStoreFactoryImpl import TaskScraperManagerFactoryStoreFactoryImpl
